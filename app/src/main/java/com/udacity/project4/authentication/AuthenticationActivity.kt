c9613e27576d46194bbbd31153b2f2211492c18b
Jinsu Baek <jinsu350@gmail.com>
2021-06-29 17:55:29 +0900

    Sign in & up with Firebase UI

--- app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -1,8 +1,17 @@
 package com.udacity.project4.authentication
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthMethodPickerLayout
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 import com.udacity.project4.R
+import com.udacity.project4.locationreminders.RemindersActivity
 
 /**
  * This class should be the starting point of the app, It asks the users to sign in / register, and redirects the
@@ -14,11 +23,33 @@ class AuthenticationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authentication)
 //         TODO: Implement the create account and sign in using FirebaseUI, use sign in using email and sign in using Google
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
 
-//          TODO: If the user was authenticated, send him to RemindersActivity
+        startActivityForResult(AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .setTheme(R.style.FirebaseTheme)
+            .build(), RC_SIGN_IN)
+    }
 
-//          TODO: a bonus is to customize the sign in flow to look nice using :
-        //https://github.com/firebase/FirebaseUI-Android/blob/master/auth/README.md#custom-layout
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == RC_SIGN_IN) {
+            if(resultCode == Activity.RESULT_OK) {
+                val user = FirebaseAuth.getInstance().currentUser
+                val dataIntent = Intent().apply {
+                    putExtra(RemindersActivity.EMAIL, user?.email)
+                }
+                setResult(Activity.RESULT_OK, dataIntent)
+            }
+            finish()
+        }
+    }
 
+    companion object {
+        const val RC_SIGN_IN = 1;
     }
 }
--- app/src/main/java/com/udacity/project4/locationreminders/RemindersActivity.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/RemindersActivity.kt
@@ -1,14 +1,20 @@
 package com.udacity.project4.locationreminders
 
 import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.NavHostFragment
 import com.udacity.project4.R
+import com.udacity.project4.authentication.AuthenticationActivity
 import kotlinx.android.synthetic.main.activity_reminders.*
 
 /**
@@ -16,10 +22,26 @@ import kotlinx.android.synthetic.main.activity_reminders.*
  */
 class RemindersActivity : AppCompatActivity() {
 
+    companion object {
+        const val AUTH_REQUEST = 1
+        const val EMAIL = "EMAIL"
+    }
+
+    lateinit var pref: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reminders)
 
+        pref = getSharedPreferences("com.udacity.project4", MODE_PRIVATE)
+
+        // 로그인이 안되었을때
+        if(pref.getString(EMAIL, "").isNullOrEmpty()) {
+            val authIntent = Intent(this, AuthenticationActivity::class.java)
+            startActivityForResult(authIntent, AUTH_REQUEST)
+        } else {
+            requestPermissions()
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -28,7 +50,39 @@ class RemindersActivity : AppCompatActivity() {
                 (nav_host_fragment as NavHostFragment).navController.popBackStack()
                 return true
             }
+            R.id.logout -> {
+                val authIntent = Intent(this, AuthenticationActivity::class.java)
+                startActivityForResult(authIntent, AUTH_REQUEST)
+                pref.edit().putString(EMAIL, "").apply()
+                Toast.makeText(this, "로그아웃 되었습니다", Toast.LENGTH_SHORT).show()
+            }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        Log.i("ReminderActivity", "onActivityResult 호출")
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == AUTH_REQUEST && resultCode == Activity.RESULT_OK) {
+            val email = data?.getStringExtra(EMAIL)
+            pref.edit().putString(EMAIL, email).apply()
+            Toast.makeText(this, "$email 로그인 되었습니다", Toast.LENGTH_LONG).show()
+            requestPermissions()
+        } else {
+            finish()
+        }
+    }
+
+    private fun requestPermissions() {
+
+        val permissions = arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_BACKGROUND_LOCATION,
+            Manifest.permission.INTERNET,
+            Manifest.permission.WAKE_LOCK
+        )
+            if(ContextCompat.checkSelfPermission(this, permissions[0]) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, permissions, 0)
+            }
     }
 }
