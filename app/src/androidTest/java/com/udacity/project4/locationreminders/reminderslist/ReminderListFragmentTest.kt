e8588cf9107d45aedf85f84b4512ea619cdf1243
Jinsu Baek <jinsu350@gmail.com>
2021-07-06 17:00:08 +0900

    Test for ReminderListFragment

--- app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
+++ app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
@@ -5,14 +5,16 @@ import android.os.Bundle
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
-import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
 import com.udacity.project4.R
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.hamcrest.core.IsNot.not
 import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -26,6 +28,39 @@ import org.mockito.Mockito.verify
 class ReminderListFragmentTest {
 
 //    TODO: test the navigation of the fragments.
+    @Test
+    fun clickFab_navigateToAddReminder() = runBlockingTest {
+        // On ReminderListFragment
+        val scenario = launchFragmentInContainer<ReminderListFragment>(Bundle(), R.style.AppTheme)
+
+        val navController = mock(NavController::class.java)
+        scenario.onFragment {
+            Navigation.setViewNavController(it.view!!, navController)
+        }
+
+        onView(withId(R.id.addReminderFAB)).perform(click())
+
+        verify(navController).navigate(ReminderListFragmentDirections.toSaveReminder())
+    }
+
 //    TODO: test the displayed data on the UI.
+    @Test
+    fun reminderList_DisplayedData() = runBlockingTest {
+        val scenario = launchFragmentInContainer<ReminderListFragment>(Bundle(), R.style.AppTheme)
+    var isReminderEmpty: Boolean = true
+        scenario.onFragment {
+            isReminderEmpty = it._viewModel.remindersList.value?.isNotEmpty() == true
+        }
+
+    if(isReminderEmpty) {
+        onView(withId(R.id.noDataTextView)).check(matches(not(isDisplayed())))
+    } else {
+        onView(withId(R.id.noDataTextView)).check(matches(isDisplayed()))
+    }
+
+    onView(withId(R.id.addReminderFAB)).check(matches(isDisplayed()))
+}
+
 //    TODO: add testing for the error messages.
+
 }
