5d4c1663caeb244ac8a9239a423fbc40cbc43729
Jinsu Baek <jinsu350@gmail.com>
2021-07-16 09:30:37 +0900

    Created LocationUtils to check permission anywhere

---
+++ app/src/main/java/com/udacity/project4/utils/LocationUtils.kt
@@ -0,0 +1,35 @@
+package com.udacity.project4.utils
+
+import android.app.Activity
+import android.content.IntentSender
+import android.util.Log
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.LocationSettingsRequest
+
+
+const val REQUEST_TURN_DEVICE_LOCATION_ON = 1
+private const val TAG = "LocationUtils"
+
+fun checkDeviceLocationSettings(activity: Activity) {
+
+    val locationRequest = LocationRequest.create().apply {
+        priority = LocationRequest.PRIORITY_LOW_POWER
+    }
+    val builder = LocationSettingsRequest.Builder().addLocationRequest(locationRequest)
+    val settingsClient = LocationServices.getSettingsClient(activity)
+    val locationSettingsResponseTask = settingsClient.checkLocationSettings(builder.build())
+    locationSettingsResponseTask.addOnFailureListener { exception ->
+        if(exception is ResolvableApiException) {
+            try {
+                exception.startResolutionForResult(activity, REQUEST_TURN_DEVICE_LOCATION_ON)
+            } catch (sendEx: IntentSender.SendIntentException) {
+                Log.d(TAG, "Error getting location settings resolution: " + sendEx.message)
+            }
+        } else {
+            Log.d(TAG, "Error getting location settings resolution")
+        }
+    }
+
+}
